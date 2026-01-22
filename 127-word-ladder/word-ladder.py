2ac271d7a0ef09d3d7c113e42159bc7552cb9319
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-01-22 07:49:31 +0530

    Time: 275 ms (62.46%) | Memory: 13.2 MB (55.89%) - LeetSync

---
+++ 127-word-ladder/word-ladder.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord, endWord, wordList):
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+
+        queue = deque()
+        queue.append((beginWord, 1))
+
+        while queue:
+            word, length = queue.popleft()
+
+            if word == endWord:
+                return length
+
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    next_word = word[:i] + c + word[i+1:]
+
+                    if next_word in wordSet:
+                        wordSet.remove(next_word)
+                        queue.append((next_word, length + 1))
+
+        return 0
