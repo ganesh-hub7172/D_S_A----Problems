56264df650f7dff2be66cd97ad45aa03aaf5898e
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-01-26 08:50:24 +0530

    Time: 819 ms (36.06%) | Memory: 43.9 MB (32.48%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -1,32 +1,29 @@
 class Solution:
     def minCut(self, s):
         n = len(s)
-
-        # palindrome table
-        isPal = [[False] * n for _ in range(n)]
-
+        
+        # palindrome[i][j] = True if s[i:j+1] is palindrome
+        palindrome = [[False] * n for _ in range(n)]
+        
         for i in range(n):
-            isPal[i][i] = True
-
+            palindrome[i][i] = True
+        
         for length in range(2, n + 1):
             for i in range(n - length + 1):
                 j = i + length - 1
                 if s[i] == s[j]:
-                    if length == 2:
-                        isPal[i][j] = True
-                    else:
-                        isPal[i][j] = isPal[i + 1][j - 1]
+                    if length == 2 or palindrome[i + 1][j - 1]:
+                        palindrome[i][j] = True
 
-        # dp array
         dp = [0] * n
-
+        
         for i in range(n):
-            if isPal[0][i]:
+            if palindrome[0][i]:
                 dp[i] = 0
             else:
-                dp[i] = float('inf')
-                for j in range(1, i + 1):
-                    if isPal[j][i]:
-                        dp[i] = min(dp[i], dp[j - 1] + 1)
+                dp[i] = i  # worst case: cut every character
+                for j in range(i):
+                    if palindrome[j + 1][i]:
+                        dp[i] = min(dp[i], dp[j] + 1)
 
-        return dp[n - 1]
+        return dp[-1]
